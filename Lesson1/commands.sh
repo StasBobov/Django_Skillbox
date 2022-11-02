e65da3803721219dae235ff0d15dbaa7078f8d5a
Stasyan <chousetheone@gmail.com>
2022-11-02 21:11:28 +0200

    enter

--- Lesson1/commands.sh
+++ Lesson1/commands.sh
@@ -1,2 +1,16 @@
 # создать каркас и файлы для работы приложения
 django-admin startproject todo
+# табличку какую-то выводит
+ls
+# перейти в директорию проекта 'todo'
+cd todo/
+# создаём новое приложение (создаёт папку tasks)
+django-admin startapp tasks
+# миграция данных в базу данных (создаёт файл manage.py)
+python manage.py migrate
+# создаём пользователя
+python manage.py createsuperuser
+# запускаем сервер
+python manage.py runserver
+# остановить сервер
+CTRL + C
